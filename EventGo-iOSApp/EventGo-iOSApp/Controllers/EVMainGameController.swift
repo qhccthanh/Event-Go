077b8f565b65b181fe25718dce5c4296303870aa
Quach Ha Chan Thanh <qhcthanh@gmail.com>
2017-04-05 17:17:01 +0700

    [Map-Marker] Example Prototype EVMarkerManager

--- EventGo-iOSApp/EventGo-iOSApp/Controllers/EVMainGameController.swift
+++ EventGo-iOSApp/EventGo-iOSApp/Controllers/EVMainGameController.swift
@@ -9,6 +9,44 @@
 import UIKit
 import GoogleMaps
 
+class EVMarker: NSObject {
+    
+    var id: String!
+    
+}
+
+class EVMakerManager: NSObject {
+    
+    static var shareManager: EVMakerManager = EVMakerManager()
+    
+    var markers: [EVMarker] = []
+    var mapView: GMSMapView!
+    
+    private override init() {
+         super.init()
+        
+    }
+    
+    public func addMarker(_ marker: EVMarker) {
+        // Add mapview
+    }
+    
+    public func addMarker(markers: [EVMarker]) {
+        // Add mapview
+    }
+    
+    public func deleteMarker(_ marker: EVMarker) {
+        
+    }
+    
+    public func deleteMarker(id: String) {
+        
+    }
+    
+    // de
+    
+}
+
 class EVMainGameController: UIViewController {
 
     @IBOutlet var mainMapView: GMSMapView!
@@ -17,8 +55,7 @@ class EVMainGameController: UIViewController {
         
         let camera = GMSCameraPosition.camera(withLatitude: 10.756927 , longitude: 106.684670, zoom: 13.0)
         self.mainMapView.camera = camera
-    
-        
+         
     }
 
     override func didReceiveMemoryWarning() {
